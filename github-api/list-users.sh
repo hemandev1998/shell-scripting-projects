01f9a0c1daa2f8ded772d43be5dd158632990e00
hemandev1998 <152362270+hemandev1998@users.noreply.github.com>
2024-05-23 18:11:08 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -35,7 +35,10 @@ function list_users_with_read_access {
         echo "$collaborators"
     fi
 }
-
+function helper{
+    expected_cmd_arg=2
+    if [ $# -ne $expected_cmd_args]; then
+        echo "Please execute the script with required args"
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
